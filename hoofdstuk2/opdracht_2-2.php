d90a233f70005cc6389063da7c0e9824173f0902
rachel1999 <rachel.sarikaya@edu-kw1c.nl>
2020-02-11 12:13:11 +0100

    added comments and finished opdracht_2-2.php

--- hoofdstuk2/opdracht_2-2.php
+++ hoofdstuk2/opdracht_2-2.php
@@ -3,7 +3,7 @@
  User: Rachel1999
  Date: 5-2-2020
  Time: 14:38
- File: template.php
+ File: opdracht_2-2.php
 */
 ?>
 <html lang="en">
@@ -15,14 +15,17 @@
     </title>
 </head>
 <body>
+<!-- Uitwerking PHP opdrachten -->
 <header>
     <h1>
         <?php echo "Uitwerking van PHP-opdrachten"; ?>
     </h1>
 </header>
+<!-- Side menu -->
 <aside>
     <h2>Menu</h2>
     <ul>
+        <!-- Opdrachten hoofdstuk 2 -->
         <li>Hoofdstuk 2
             <ul>
                 <li>
@@ -33,6 +36,8 @@
                 </li>
             </ul>
         </li>
+
+        <!-- Opdrachten hoofdstuk 3 -->
         <li>Hoofdstuk 3
             <ul>
                 <li>
@@ -46,6 +51,8 @@
                 </li>
             </ul>
         </li>
+
+        <!-- Opdrachten hoofdstuk 4 -->
         <li>Hoofdstuk 4
             <ul>
                 <li>
@@ -62,6 +69,8 @@
                 </li>
             </ul>
         </li>
+
+        <!-- Opdrachten hoofdstuk 5 -->
         <li>Hoofdstuk 5
             <ul>
                 <li>
@@ -83,5 +92,39 @@
         </li>
     </ul>
 </aside>
+<!-- Uitwerkingen opdracht 2-2 -->
+<main id="wrapper">
+    <?php
+    // Losse stukken tekst in variabele
+        $text1 = "Hallo";
+        $text2 = " een makkelijke taal";
+        $text3 = " toch zo'n makkelijke taal ";
+        $text4 = " wat is ";
+        $text5 = " PHP ";
+        $text6 = " Nooit gedacht dat ";
+        $text7 = " De installatie is best ingewikkeld";
+        $text8 = " Fijn ";
+        $text9 = "?";
+        $text10 = ".";
+        $text11 = ",";
+        $text12 = "<br>";
+        $text13 = " is";
+        $text14 = " Vind je niet";
+        $text15 = " toch";
+        $text16 = " dat ";
+
+        // Taak 2
+        echo "<h2> Taak 2 </h2>";
+        echo "<p>".$text1 . $text11 .  $text4 . $text5 . $text15 . $text2.$text10. $text12. $text7.$text10 . $text8 . $text15.$text9 . $text12 .$text6 . $text5 . $text3 . $text13 . $text10."</p>";
+
+        // Taak 3
+        echo "<h2> Taak 3 </h2>";
+        echo "<p>".$text1 . $text11 . $text12 . $text8 . $text16 . $text5 . $text3 . $text13 . $text10 . $text12 . $text7 . $text10 . $text14 . $text9 ."</p>"
+    ?>
+    <p>
+        <!-- Link naar hoofdpagina -->
+        <a href="../index.php">Terug naar hoofdpagina</a>
+    </p>
+</main>
 </body>
 </html>

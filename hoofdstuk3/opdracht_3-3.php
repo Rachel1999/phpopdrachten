01143043672842d19635a198f2f9e4589b334811
rachel1999 <rachel.sarikaya@edu-kw1c.nl>
2020-02-19 14:32:33 +0100

    added and finished opdracht_3-3.php

---
+++ hoofdstuk3/opdracht_3-3.php
@@ -0,0 +1,58 @@
+<?php
+/*
+ User: Rachel1999
+ Date: 11-2-2020
+ Time: 12:32
+ File: opdracht_3-3.php
+*/
+?>
+
+<!-- Header en menu -->
+<?php
+    include "../includes/header.php";
+    include "../includes/menu.php";
+?>
+
+<!-- Uitwerkingen opdracht 3-3 -->
+<main id="wrapper">
+    <h2>
+        Includes
+    </h2>
+    <p>
+        <!-- Taak 1 -->
+        &#9745; Je hebt in de map includes aangemaakt met daarin het footer gedeelte van je template. <br>
+
+        <!-- Taak 2 -->
+        &#9745; Je hebt in de map includes aangemaakt met daarin het header gedeelte van je template. <br>
+
+        <!-- Taak 3 -->
+        &#9745; Je hebt de gegeven variabelen aangemaakt in variabelen.php <br>
+
+        <!-- Taak 4 -->
+        &#9745; Je hebt het gegeven bestand aangemaakt <br>
+
+        <!-- Taak 5 -->
+        &#9745; De header code is vervangen door de include code <br>
+
+        <!-- Taak 6 -->
+        &#9745; Variabelen.php is geinclude in footer.php en de footer ziet eruit zoals onder kopje “Visuele weergave” <br>
+
+        <!-- Taak 7 -->
+        &#9745; De footer code is vervangen door de include code <br>
+
+        <!-- Taak 8 -->
+        &#9745; De wijzigingen zijn ook in je template doorgevoerd <br>
+    </p>
+
+    <p>
+        <!-- Link naar hoofdpagina -->
+        <a href="../index.php">Terug naar hoofdpagina</a>
+    </p>
+
+    <!-- Footer -->
+    <?php
+        include "../includes/footer.php";
+    ?>
+</main>
+</body>
+</html>

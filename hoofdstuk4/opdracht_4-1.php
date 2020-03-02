ea73f4f1da4a21b77cce434b3a8d68998dabb833
rachel1999 <rachel.sarikaya@edu-kw1c.nl>
2020-03-02 11:37:26 +0100

    added checklist for opdracht_4-1.php

---
+++ hoofdstuk4/opdracht_4-1.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ User: Rachel1999
+ Date:
+ Time:
+ File: opdracht_4-1.php
+*/
+?>
+
+<!-- Header en menu -->
+<?php
+    include "../includes/header.php";
+    include "../includes/menu.php";
+?>
+
+<!-- Uitwerkingen opdracht 4-1 -->
+<main id="wrapper">
+    <h1>
+        Datum en tijd instellen in de footer
+    </h1>
+    <p>
+        <!-- Taak 1 -->
+        &#9745; De aangegeven variabele is gedeclareerd in het aangegeven bestand. <br>
+
+        <!-- Taak 2 -->
+        &#9745; De functie date() is gebruikt om het huidige uur op te halen. <br>
+                Het uur van de dag is opgeslagen in de variabele $uur. <br>
+
+        <!-- Taak 3 -->
+        &#9745; Deze footer is in iedere opdracht die je tot nu toe gemaakt hebt te zien. <br>
+    </p>
+
+    <p>
+        <!-- Link naar hoofdpagina -->
+        <a href="../index.php">Terug naar hoofdpagina</a>
+    </p>
+
+    <!-- Footer -->
+    <?php
+        include "../includes/footer.php";
+    ?>
+</main>
+</body>
+</html>

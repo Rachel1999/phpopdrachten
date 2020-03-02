877f930ae1b8162e30b43c475ad653bca2942b82
rachel1999 <rachel.sarikaya@edu-kw1c.nl>
2020-03-02 11:33:42 +0100

    Added assignment 4-1 in footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,32 @@
 <footer>
         <?php
             include "variabelen.php";
-            echo "<p> &copy; " . $year . " " . $name . "</p";
+
+            // Time
+            date_default_timezone_set ("Europe/Amsterdam");
+            $uur = date("H");
+
+            // Bericht aan de hand van het uur
+            if ($uur >= 0 && $uur < 5)
+            {
+                $uur = "Goedennacht";
+            }
+            elseif ($uur >= 5 && $uur < 12)
+            {
+                $uur = "Goedenochtend";
+            }
+
+            elseif ($uur >= 12 && $uur < 17)
+            {
+                $uur = "Goedenmiddag";
+            }
+
+            elseif ($uur >= 17 && $uur < 24)
+            {
+                $uur = "Goedenavond";
+            }
+
+            // Tekst in footer
+            echo "<p>" . $uur ." &copy; " . $year . " " . $name . "</p";
         ?>
 </footer>

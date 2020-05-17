07e6b71ae94d4044e464051a955b855966c342b2
rachel1999 <rachel.sarikaya@edu-kw1c.nl>
2020-05-17 16:27:28 +0200

    Oplevering Opdracht7.2

--- hoofdstuk7/opdracht7.1/opdracht_7-1.php
+++ hoofdstuk7/opdracht7.1/opdracht_7-1.php
@@ -1,4 +1,8 @@
+<!-- Header en menu -->
 <?php
+include "../../includes/header.php";
+include "../../includes/menu.php";
+
 // Open de database connectie en ODBC driver
 try
 {
@@ -16,7 +20,7 @@ echo "database connectie gelukt";
 try
 {
     // Query schrijven
-    $sql = 'SELECT * FROM joke';
+    $sql = "SELECT * FROM joke WHERE jokedate < '2020'";
     // Query uitvoeren
     $result = $pdo->query($sql);
 }
@@ -34,6 +38,54 @@ while ($row = $result->fetch(PDO::FETCH_ASSOC))
     $aJokes[] = $row;
 }
 // Tonen van de inhoud van aJokes
-var_dump($aJokes);
+//echo "<pre";
+//var_dump($aJokes);
+//echo "</pre";
+
+
 
 ?>
+<!-- Uitwerkingen opdracht 7.2 -->
+<main id="wrapper">
+    <h1>
+        Grappen
+    </h1>
+
+    <!-- Tabel met alle grappen -->
+    <table id="jokeTable">
+        <thead>
+            <th>ID</th>
+            <th>Joketext</th>
+            <th>Jokeclou</th>
+            <th>Jokedate</th>
+        </thead>
+        <?php
+        foreach($aJokes as $grap)
+        {
+
+            echo "<tr><td>";
+            echo $grap['id'];
+            echo "</td><td>";
+            echo $grap['joketext'];
+            echo "</td><td>";
+            echo $grap['jokeclou'];
+            echo "</td><td>";
+            echo $grap['jokedate'];
+            echo "</td></tr>";
+        };
+        ?>
+    </table>
+
+    <p>
+        <!-- Link naar hoofdpagina -->
+        <a href="../../index.php">Terug naar hoofdpagina</a>
+    </p>
+
+<?php
+// Footer
+include "../../includes/footer.php";
+?>
+
+</main>
+</body>
+</html>
